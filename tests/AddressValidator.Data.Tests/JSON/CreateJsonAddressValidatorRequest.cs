62afc32a81e607b6ff061acdc0040ee207a8e1d3
Jamie Bowman <mrjamiebowman@gmail.com>
2021-01-25 12:12:50 -0500

    updated refs to json and created a unit test to create json request

--- tests/AddressValidator.Data.Tests/JSON/CreateJsonAddressValidatorRequest.cs
+++ tests/AddressValidator.Data.Tests/JSON/CreateJsonAddressValidatorRequest.cs
@@ -3,10 +3,10 @@ using Xunit;
 
 namespace AddressValidator.Data.Tests
 {
-    public class UnitTest1
+    public class CreateJsonAddressValidatorRequest
     {
         [Fact]
-        public void Test1()
+        public void CreateJsonAddressValidatorRequestTest()
         {
 
         }
