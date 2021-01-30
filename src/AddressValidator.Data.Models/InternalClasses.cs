2e51a4d7a705b4da64224909851af797075087ea
Jamie Bowman <mrjamiebowman@gmail.com>
2021-01-29 20:45:32 -0500

    internal abstract class

---
+++ src/AddressValidator.Data.Models/InternalClasses.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("AddressValidator.Api")]
+[assembly: InternalsVisibleTo("AddressValidator.Data")]
+[assembly: InternalsVisibleTo("AddressValidator.Data.Tests")]
+namespace AddressValidator.Data.Models
+{
+    internal abstract class InternalClasses
+    {
+
+    }
+}
