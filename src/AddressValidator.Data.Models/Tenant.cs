f61da68e00b2c1d6db8a43c76c2e8d08bc15c76c
Jamie Bowman <mrjamiebowman@gmail.com>
2021-01-29 21:14:37 -0500

    updated tenant class

---
+++ src/AddressValidator.Data.Models/Tenant.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+using System.Text.Json.Serialization;
+
+namespace AddressValidator.Data.Models
+{
+    internal class Tenant : InternalClasses
+    {
+        [JsonIgnore]
+        [Required]
+        public int? CompanyId { get; set; }
+
+        [JsonIgnore]
+        [Required]
+        public int? ApplicationId { get; set; }
+    }
+}
