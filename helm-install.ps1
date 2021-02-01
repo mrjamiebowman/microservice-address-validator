979239ea42346114036855dba27a0926ac181de9
Jamie Bowman <mrjamiebowman@gmail.com>
2021-01-31 20:31:02 -0500

    updates to helm

--- helm-install.ps1
+++ helm-install.ps1
@@ -1,1 +1,5 @@
-helm install core-addressvalidator .\charts\addressvalidator\
+# create namespace for core microservices
+kubectl create namespace corems
+
+# install helm charts
+helm install core-addressvalidator .\charts\addressvalidator\ -n corems
